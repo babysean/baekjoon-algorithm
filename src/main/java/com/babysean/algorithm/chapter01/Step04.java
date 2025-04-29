122700b025dc2fcb4d7ad5dbc1e23b76052038be
박상훈 <sh.park@dongwha.com>
2025-04-29 09:16:53 +0900

    feat: 고양이를 출력하는 프로그램
    - \\로 입력해야 \로 출력된다.

---
+++ src/main/java/com/babysean/algorithm/chapter01/Step04.java
@@ -0,0 +1,12 @@
+package com.babysean.algorithm.chapter01;
+
+public class Step04 {
+
+    public static void main(String[] args) {
+        System.out.println("\\      /\\");
+        System.out.println(" )   (  ')");
+        System.out.println("(   /   )");
+        System.out.println(" \\ (__) |");
+    }
+
+}
